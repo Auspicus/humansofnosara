92f799ed51a0daaa578e71fa1c0d076a7fb53159
edisch <elijahdisch@gmail.com>
2017-09-27 00:42:52 -0600

    Added volunteer page, forms, js gulp task, shortcodes

---
+++ content/page/volunteer.md
@@ -0,0 +1,24 @@
+{
+  "call_to_action": [
+  ],
+  "date": "2017-09-26T13:25:52-06:00",
+  "title": "Become a volunteer",
+  "url": "/volunteer"
+}
+# Become a volunteer
+
+Start writing stories, taking photos, interviewing, translating or writing code for us by filling out the form below. We appreciate that you are taking the time to contact us. We will try to get back to you as soon as we can to let you know how you can help.
+
+{{% form-two-column POST "https://formspree.io/humansofnosara@gmail.com" true %}}
+<div class="form-two-column__field">{{< field "Email" email email field-email true >}}</div>
+<div class="form-two-column__field">{{< field "Confirm Email" email email-confirm field-email-confirm true >}}</div>
+<div class="form-two-column__field">{{< field "First Name" text first-name field-first-name true >}}</div>
+<div class="form-two-column__field">{{< field "Last Name" text last-name field-last-name true >}}</div>
+<div class="form-two-column__field">{{< field "Phone Number" tel phone field-phone true >}}</div>
+<div class="form-two-column__field">{{< field "Role (eg. photographer)" text role field-role true >}}</div>
+{{< field-hidden _next "//www.humansofnosara.org/" >}}
+{{< field-hidden _language en >}}
+{{< field-hidden _gotcha "" >}}
+{{< field-hidden _subject "Volunteer - Humans of Nosara" >}}
+<div class="form-two-column__submit">{{< submit "Submit" >}}</div>
+{{% /form-two-column %}}
--- themes/humans-of-nosara/gulpfile.js
+++ themes/humans-of-nosara/gulpfile.js
@@ -4,38 +4,76 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var cleanCSS = require('gulp-clean-css');
 var exec = require('child_process').exec;
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var globby = require('globby');
+var through = require('through2');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('watch', ['hugo'], function () {
   gulp.watch([
     '../../content/**/*.md',
+    '../../config.json',
     './layouts/**/*.html',
     './i18n/*.json',
-    './static/**/*'
+    './components/**/*.scss',
+    './components/**/*.js',
+    './static/**/*',
+    './scss/**/*.scss'
   ], ['hugo']);
-  gulp.watch([
-    './scss/**/*.scss',
-    './layouts/**/*.scss'
-  ], ['styles']);
 });
 
-gulp.task('hugo', ['styles'], function (done) {
-  exec('hugo --source "../../"', function (err, stdout, stderr) {
-    if (err)
-      console.log(stdout);
-    else if (stderr)
-      console.log(stdout);
-    done();
+gulp.task('hugo', function (done) {
+  new Promise(function (resolve, reject) {
+    gulp
+    .src('./scss/styles.scss')
+    .pipe(sassGlob())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer(['last 3 versions']))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./static/css'))
+    .on('end', resolve);
+  })
+  .then(function () {
+    return new Promise(function (resolve, reject) {
+      var bundledStream = through();
+      bundledStream
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./static/js/'));
+
+      globby(['./components/**/*.js'])
+      .then(function(entries) {
+        var b = browserify({
+          entries: entries,
+          debug: true
+        });
+        b.bundle().pipe(bundledStream);
+      }).catch(function(err) {
+        bundledStream.emit('error', err);
+      });
+  
+      resolve(bundledStream);
+    });
+  })
+  .then(function () {
+    exec('hugo --source "../../"', function (err, stdout, stderr) {
+      if (err)
+        console.log(stdout);
+      else if (stderr)
+        console.log(stdout);
+      done();
+    });
+  })
+  .catch(function (err) {
+    console.error(err);
+    process.exit(1);
   });
-});
-
-gulp.task('styles', function () {
-  gulp
-  .src('./scss/styles.scss')
-  .pipe(sassGlob())
-  .pipe(sass().on('error', sass.logError))
-  .pipe(autoprefixer(['last 3 versions']))
-  .pipe(cleanCSS())
-  .pipe(gulp.dest('./static/css'));
 });
 
 gulp.task('default', ['watch']);
